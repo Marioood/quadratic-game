0298a4080f144fd0b7dffa37e1dd95ba27049840
Marioood <121731808+Marioood@users.noreply.github.com>
2024-06-05 12:38:11 -0500

    sorry i forgot to update this

--- shared.js
+++ shared.js
@@ -1,42 +1,58 @@
-//logic shared between the simple and complex problems
+//non-game || setup logic
+let facsHide = false;
 function setup() {
-	const f1 = document.getElementById("f1");
-	const f2 = document.getElementById("f2");
+	addInputUpdater("f1");
+	addInputUpdater("f2");
+	addInputUpdater("c1");
+	addInputUpdater("c2");
 	
-	f1.addEventListener("input", updateSmp);
-	f2.addEventListener("input", updateSmp);
+	document.getElementById("faccellinput").addEventListener("input", event => {
+		const input = Math.abs(document.getElementById("faccellinput").value);
+		const output = document.getElementById("faccellcell");
+		let error = true;
+		let warningTxt;
+		
+		while(output.firstChild) {
+			output.removeChild(output.firstChild);
+		}
+		//99999900 most entries
+		if(input > 1e+8) {
+			warningTxt = "number to big";
+		} else if(input == 0) {
+			warningTxt = "cant do zero";
+		} else if(input != Math.round(input)) {
+			warningTxt = "number not whole";
+		} else {
+			arrayToList(factorsOf(input), output);
+			error = false;
+		}
+		
+		if(error) {
+			const warning = document.createElement("p");
+			warning.innerText = warningTxt;
+			output.appendChild(warning);
+		}
+	});
+	
 	this.addEventListener("keydown", event => {
 		//check the answer if enter is pressed
 		if(event.keyCode == 13) {
 			checkAnswer();
 		}
 	});
-	//firefox doesnt clear input boxes when the page is reloaded
-	f1.value = "";
-	f2.value = "";
-	
+	newProblem();
 	updateSmp();
 	updateStd();
 	createFactorTable();
 	createHelpTable();
+	toggleFactorTable();
 }
 
-function addInsert(num) {
-	if(num == 0) {
-		return ``;
-	} else {
-		return ` ${num > 0 ? "+" : "-"} ${Math.abs(num)}`;
-	}
-}
-
-function varInsert(num) {
-	if(num == 0) {
-		return addInsert(num);
-	} else if(num == 1) {
-		return "x";
-	} else {
-		return addInsert(num) + "x";
-	}
+function addInputUpdater(id) {
+	const element = document.getElementById(id);
+	element.addEventListener("input", updateSmp);
+	//firefox doesnt clear input boxes when the page is reloaded
+	element.value = null;
 }
 
 function factorsOf(num) {
@@ -69,7 +85,7 @@ function createFactorTable() {
 		const tableRow = document.createElement("tr");
 		for(let dat = 1; dat <= FACTOR_TABLE_WIDTH; dat++) {
 			const tableData = document.createElement("td");
-			const list = document.createElement("ul");
+			//const list = document.createElement("ul");
 			arrayToList(factorsOf(dat + (FACTOR_TABLE_WIDTH * row)), tableData);
 			tableRow.appendChild(tableData);
 		}
@@ -91,15 +107,21 @@ function arrayToList(array, element) {
 
 function toggleFactorTable() {
 	const table = document.getElementById("table-container");
-
-	if(table.style.display == "table") {
-		table.style.display = "none";
+	const facButton = document.getElementById("fac-button");
+	
+	if(facsHide) {
+		table.className = "table-container";
+		facButton.innerText = "Hide Factor Table";
+		facsHide = false;
 	} else {
-		table.style.display = "table";
+		table.className = "hidden";
+		facButton.innerText = "Show Factor Table";
+		facsHide = true;
 	}
 }
 
 function createHelpTable() {
+	//just manually write this in html, its pointless as js
 	const helpTable = document.getElementById("help-container");
 	for(let row = 0; row < 3; row++) {
 		const newRow = document.createElement("div");
@@ -107,6 +129,7 @@ function createHelpTable() {
 			const newInput = document.createElement("input");
 			newInput.className = "help-input";
 			newInput.type = "number";
+			newInput.id = `h${dat + 1 + (row * 3)}`;
 			newRow.appendChild(newInput);
 		}
 		helpTable.appendChild(newRow);
@@ -114,5 +137,34 @@ function createHelpTable() {
 	const sumBox = document.createElement("input");
 	sumBox.className = "help-input help-sum";
 	sumBox.type = "number";
+	sumBox.id = "h10";
 	helpTable.appendChild(sumBox);
+}
+
+function clear() {
+	/*const help = document.getElementById("help-container");
+	for(const kiddie of help.children) {
+		kiddie.value = null;
+	}*/
+	const set = (id) => {
+		const element = document.getElementById(id);
+		element.value = null;
+	}
+	//function inputs
+	set("f1");
+	set("f2");
+	set("c1");
+	set("c2");
+	updateSmp();
+	//help table
+	set("h1");
+	set("h2");
+	set("h3");
+	set("h4");
+	set("h5");
+	set("h6");
+	set("h7");
+	set("h8");
+	set("h9");
+	set("h10");
 }
--- simple.js
+++ simple.js
@@ -1,46 +1,127 @@
 //game logic
-
-let ansFac1, ansFac2;
-let probFac1 = 4;
-let probFac2 = -8;
+const DEBUG = false;
+let aF1, aF2, aC1, aC2;
+let pF1 = -4;
+let pF2 = 4;
+let pC1 = 1;
+let pC2 = 3;
 
 function updateSmp() {
 	const smp = document.getElementById("smp");
 	//thinks im trying to concatenate these when checking the sum when not converted to a numbah
-	ansFac1 = Number(document.getElementById("f1").value);	
-	ansFac2 = Number(document.getElementById("f2").value);
-	//if an x is alone, there shouldn't be parenthesis (just so it looks nicer)
-	smp.innerHTML = `(x${addInsert(ansFac1)})(x${addInsert(ansFac2)})`;
+	aF1 = Number(document.getElementById("f1").value);	
+	aF2 = Number(document.getElementById("f2").value);
+	aC1 = Number(document.getElementById("c1").value);	
+	aC2 = Number(document.getElementById("c2").value);
+	//this is complicated, but is necessary for pretty equations
+	let txt = enclose(`${coefInsert(aC1)}${addInsert(aF1)}`) + enclose(`${coefInsert(aC2)}${addInsert(aF2)}`);
+	if(txt == "xx") {
+		smp.innerHTML = "x²"
+	} else {
+		smp.innerHTML = txt;
+	}
 }
-
 
 function updateStd() {
 	const std = document.getElementById("std");
 
-	std.innerHTML = `x²${varInsert(probFac1 + probFac2)} ${addInsert(probFac1 * probFac2)}`;
+	std.innerHTML = `${coefInsert(pC1 * pC2)}²${varInsert((pF1 * pC1) + (pF2 * pC2))} ${addInsert(pF1 * pF2)}`;
+	if(DEBUG) {
+		std.innerHTML += ` : (${coefInsert(pC1)}${addInsert(pF1)})` + `(${coefInsert(pC2)}${addInsert(pF2)})`;
+	}
 }
 
 function checkAnswer() {
-	const MAX_PROBLEM = 64;
-	const MIN_PROBLEM = 16;
-	let sumCorrect = ansFac1 + ansFac2 == probFac1 + probFac2;
-	let factorCorrect = ansFac1 * ansFac2 == probFac1 * probFac2;
+	const sumCorrect = aF1 + aF2 == pF1 + pF2;
+	const factorCorrect = (aF1 * aC1) + (aF2 * aC2) == (pF1 * pC1) + (pF2 * pC2);
+	const coefCorrect = aC1 * aC2 == pC1;
 	let txt;
 	let correct = false;
+	let wrongList = new Array();
 	//maybe change how errors are detected? sometimes "correct" sums are actually wrong
-	if(sumCorrect && factorCorrect) {
+	//also the solver straight up gets stuff wrong
+	//problem: 4x² - 42x + 80 || (2x - 16)(2x - 5)
+	//answer:  (x - 8)(4x - 10)
+	//in desmos, all of these are identical
+	
+	//TODO: oh my god this is bad rewrite it
+	if(sumCorrect && factorCorrect && coefCorrect) {
 		txt = "Correct!";
-		probFac1 = Math.round(randInt(MAX_PROBLEM) - (MAX_PROBLEM / 2));
-		probFac2 = Math.round(randInt(MIN_PROBLEM) - (MIN_PROBLEM / 2));
+		newProblem();
 		updateStd();
+		clear();
 		correct = true;
-	} else if (!sumCorrect && factorCorrect) {
-		txt = "Sum is WRONG!"
-	} else if(!factorCorrect && sumCorrect) {
-		txt = "Factor is WRONG!"
 	} else {
-		txt = "Factor and sum are both WRONG!!!";
+		if(!factorCorrect) {
+			wrongList.push("sum")
+			//txt = "Sum is WRONG!"
+		}
+		if(!sumCorrect) {
+			wrongList.push("factor")
+			//txt = "Factor is WRONG!"
+		}
+		if(!coefCorrect) {
+			wrongList.push("coefficient");
+		}
+		
+		txt = wrongList.join() + " are WRONG!!!";
 	}
 
 	document.getElementById("status").innerHTML = `${txt} ${correct ? "" : "(Retry and submit)"}`;
+}
+
+function addInsert(num) {
+	if(num == 0) {
+		return "";
+	}
+	return ` ${num > 0 ? "+" : "-"} ${Math.abs(num)}`;
+}
+
+function varInsert(num) {
+	if(num == 0) {
+		return "";
+	} else if(num == 1) {
+		return "x";
+	} else if(num == -1) {
+		return "-x"
+	}
+	return `${addInsert(num)}x`;
+}
+
+function coefInsert(num) {
+	if(num == 0) {
+		return "";
+	} else if(num == 1) {
+		return "x";
+	} else if(num == -1) {
+		return "-x"
+	}
+	return `${num}x`
+}
+
+function enclose(insert) {
+	if(insert.length < 2) {
+		return insert;
+	} else {
+		return `(${insert})`
+	}
+}
+
+function newProblem() {
+	//only use even numbers, if you use odds the problem nums will be weird decimals
+	const MAX_FAC = 64;
+	const MIN_FAC = 16;
+	const MAX_COEF = 4;
+	
+	pF1 = randInt(MAX_FAC) - (MAX_FAC / 2);
+	pF2 = randInt(MIN_FAC) - (MIN_FAC / 2);
+	pC1 = randInt(MAX_COEF);// - (MAX_COEF / 2);
+	pC2 = randInt(MAX_COEF);// - (MAX_COEF / 2);
+	//coefficients of zero create funky bugs, so just fudge the numbers
+	if(pC1 == 0) {
+		pC1++;
+	}
+	if(pC2 == 0) {
+		pC2++;
+	}
 }
